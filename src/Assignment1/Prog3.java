610177906defe7165fd50328d1cbe3d3193ddf27
rahulmarslama <rahulrlama@miu.edu>
2026-02-23 22:48:42 -0600

    Added documentation for Question2.

--- src/Assignment1/Prog3.java
+++ src/Assignment1/Prog3.java
@@ -1,5 +1,11 @@
 package Assignment1;
 
+/**
+ * Prog3
+ * This program reads product records stored in a single String, where each record
+ * is separated by ":" and each column within a record is separated by ",".
+ * It extracts all product IDs (first column) and prints them to the console.
+ */
 public class Prog3 {
     void  main(String[] args)
     {
